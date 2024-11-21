1d9b34713ef0653fea75bef48c799d3132f21d27
David Kouril <david.kouril@gmail.com>
2024-11-21 13:30:18 -0500

    add click and start work on super simple cli

--- src/chx/__init__.py
+++ src/chx/__init__.py
@@ -1,2 +1,13 @@
+import click
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+def version():
+    from ._version import __version__
+    click.echo(f"{__version__}")
+
 def main() -> None:
-    print("Hello from chx!")
+    cli()
